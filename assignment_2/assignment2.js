255d8e00298af6a9314b32016785a9c4d09590fe
gap145 <35076882+gap145@users.noreply.github.com>
2018-01-17 11:24:19 -0500

    Add files via upload

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -1,21 +1,33 @@
 // Exercise #1
 // Write your solution below
 
+var content = '';
+
+for (var i = 0; i < 7; i++) {
+    content = content + '#';
+    console.log(content);
+}
 
 /* 
-Write your explanation for Exercise #1 here
+Write your explanation for Exercise #1 here 
+This code works because we have a set size for the pyramid and a starting point through the loop provided. we are able to use the content + # as a way to build out the steps of our pyramid.
+
 
 */
-
-
-
 
 
 // Exercise #2
 // Write your solution below
 
-
+function isEven(number) {
+    if (number%2 == 0)
+    return true;
+else
+    return false;
+}
 /* 
 Write your explanation for Exercise #2 here
+
+Javascript has a modulus operator %, which finds the remainder of a division. if there is no remainder after dividing by 2 the number is even. 
 
 */
